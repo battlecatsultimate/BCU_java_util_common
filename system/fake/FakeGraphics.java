a4886837e893fdc234514550de990bb1a6372694
lcy0x1 <arthurlcy0x1@gmail.com>
2020-01-17 23:54:07 -0800

    remove int... in colRect

--- system/fake/FakeGraphics.java
+++ system/fake/FakeGraphics.java
@@ -5,7 +5,7 @@ public interface FakeGraphics {
 	public static final int RED = 0, YELLOW = 1, BLACK = 2, MAGENTA = 3, BLUE = 4, CYAN = 5, WHITE = 6;
 	public static final int DEF = 0, TRANS = 1, BLEND = 2, GRAY = 3;
 
-	public void colRect(int x, int y, int w, int h, int r, int g, int b, int... a);
+	public void colRect(int x, int y, int w, int h, int r, int g, int b, int a);
 
 	public default void delete(FakeTransform at) {
 
