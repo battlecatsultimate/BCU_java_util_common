61e7bef8536089b86b9de518fd3e87734f4f7581
lcy0x1 <arthurlcy0x1@gmail.com>
2020-02-05 22:46:00 -0800

    fix direction

--- battle/attack/AttackSimple.java
+++ battle/attack/AttackSimple.java
@@ -78,11 +78,11 @@ public class AttackSimple extends AttackAb {
 
 		if (capt.size() > 0 && proc[P_VOLC][0] > 0) {
 			int dire = model.getDire();
-			int addp = proc[P_VOLC][1] + (int)(model.b.r.nextDouble()*(proc[P_VOLC][2]-proc[P_VOLC][1]));
-			double p0 = model.getPos() + dire*addp;
-			double sta = p0 - (dire==1?W_VOLC_PIERCE:W_VOLC_INNER);
-			double end = p0 + (dire==1?W_VOLC_INNER:W_VOLC_PIERCE);
-			
+			int addp = proc[P_VOLC][1] + (int) (model.b.r.nextDouble() * (proc[P_VOLC][2] - proc[P_VOLC][1]));
+			double p0 = model.getPos() + dire * addp;
+			double sta = p0 - (dire == 1 ? W_VOLC_INNER : W_VOLC_PIERCE);
+			double end = p0 + (dire == 1 ? W_VOLC_PIERCE : W_VOLC_INNER);
+
 			new ContVolcano(new AttackVolcano(this, sta, end), p0, layer, proc[P_VOLC][3]);
 		}
 	}
