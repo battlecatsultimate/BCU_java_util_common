e6f41a0aca615a492d6f7815ac928d6b47f4f063
MandarinSmell <geawonlee@naver.com>
2020-04-17 16:19:32 +0900

    construction curse

--- util/Data.java
+++ util/Data.java
@@ -37,7 +37,7 @@ public class Data {
 	public static final int[][] SE_CANNON = { { 25, 26 }, { 60 }, { 61 }, { 36, 37 }, { 65, 83 }, { 84, 85 }, { 86 }, {124} };
 
 	public static final int[] SE_ALL = { 15, 19, 20, 21, 22, 23, 24, 25, 26, 27, 36, 37, 44, 45, 50, 59, 60, 61, 65, 73,
-			74, 83, 84, 85, 86, 90, 110, 111, 112 };
+			74, 83, 84, 85, 86, 90, 110, 111, 112, 124 };
 
 	public static final int RARITY_TOT = 6;
 
