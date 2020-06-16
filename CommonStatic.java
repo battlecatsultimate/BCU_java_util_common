db9c3f63d3f5382f9bb80d9a976cbc5e1d065d6d
MandarinSmell <geawonlee@naver.com>
2020-06-17 02:23:45 +0900

    added russian

--- CommonStatic.java
+++ CommonStatic.java
@@ -167,7 +167,7 @@ public class CommonStatic {
 
 		public static final String[] LOC_CODE = { "en", "zh", "kr", "jp", "ru", "de", "fr", "nl", "es" };
 
-		public static final int[][] pref = { { 0, 3, 1, 2 }, { 1, 3, 0, 2 }, { 2, 3, 0, 1 }, { 3, 0, 1, 2 } };
+		public static final int[][] pref = { { 0, 3, 1, 2 }, { 1, 3, 0, 2 }, { 2, 3, 0, 1 }, { 3, 0, 1, 2 }, { 0, 3, 1, 2 }, { 0, 3, 1, 2 }, { 0, 3, 1, 2 } };
 
 		public static int lang = 0;
 
