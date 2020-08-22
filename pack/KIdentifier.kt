abf001dfee701628476e409d03bfd82ad45f74d0
lcy0x1 <arthurlcy0x1@gmail.com>
2020-08-22 20:28:24 +0800

    fix minor error

--- pack/KIdentifier.kt
+++ pack/KIdentifier.kt
@@ -4,6 +4,6 @@ fun <T : IndexContainer.Indexable<*, T>> Identifier<T>?.get(): T? {
     return this?.get()
 }
 
-fun <T> Identifier<*>?.getOr(cls : Class<T>): T? {
+fun <T> Identifier<*>?.getOr(cls : Class<T>): T {
     return Identifier.getOr(this, cls)
 }
