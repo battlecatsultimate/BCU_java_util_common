95d9a3d3724c2a5f7c5891b67ed075425e5203aa
iREDMe <foodrickme@gmail.com>
2021-01-11 13:54:48 -0800

    fix savage blow damage

--- battle/entity/ECastle.java
+++ battle/entity/ECastle.java
@@ -33,14 +33,12 @@ public class ECastle extends AbEntity {
 		int satk = atk.getProc().SATK.mult;
 		if (satk > 0)
 			ans *= (100 + satk) * 0.01;
+			sb.lea.add(new EAnimCont(pos, 9, effas().A_SATK.getEAnim(DefEff.DEF)));
+			CommonStatic.setSE(SE_SATK);
 		if (atk.getProc().CRIT.mult > 0) {
 			ans *= 0.01 * atk.getProc().CRIT.mult;
 			sb.lea.add(new EAnimCont(pos, 9, effas().A_CRIT.getEAnim(DefEff.DEF)));
 			CommonStatic.setSE(SE_CRIT);
-		} else if (atk.getProc().SATK.mult > 0) {
-			ans *= 0.01 * atk.getProc().SATK.mult;
-			sb.lea.add(new EAnimCont(pos, 9, effas().A_SATK.getEAnim(DefEff.DEF)));
-			CommonStatic.setSE(SE_SATK);
 		} else
 			CommonStatic.setSE(SE_HIT_BASE);
 		health -= ans;
