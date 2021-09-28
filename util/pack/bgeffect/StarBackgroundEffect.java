6646b8cc6b433b9cbcab98cc8de152c357c67ffd
MandarinSmell <mandarinsmellbcu@gmail.com>
2021-09-28 22:17:13 +0900

    add star bg effect

--- util/pack/bgeffect/StarBackgroundEffect.java
+++ util/pack/bgeffect/StarBackgroundEffect.java
@@ -13,6 +13,8 @@ import java.util.List;
 @SuppressWarnings("ForLoopReplaceableByForEach")
 public class StarBackgroundEffect extends BackgroundEffect {
     private static final int BGHeight = 510;
+    private static final int battleOffset = (int) (400 / CommonStatic.BattleConst.ratio);
+
     private static final int[][] starColors = {
             {233, 248, 255},
             {199, 249, 218},
@@ -29,8 +31,6 @@ public class StarBackgroundEffect extends BackgroundEffect {
     private final List<Integer> capture = new ArrayList<>();
 
     private int number;
-    private int len;
-    private int rangeH;
 
     @Override
     public void check() {
@@ -46,8 +46,10 @@ public class StarBackgroundEffect extends BackgroundEffect {
             int[] c = starColors[colors.get(i)];
 
             g.setComposite(FakeGraphics.BLEND, 255, 1);
-            g.colRect(convertP(positions.get(i).x, siz) + (int) rect.x, (int) (positions.get(i).y * siz), (int) Math.max(1, siz * 6), (int) Math.max(1, siz * 6), c[0], c[1], c[2], opacities.get(i));
+            g.colRect(convertP(positions.get(i).x, siz) + (int) rect.x, (int) (positions.get(i).y * siz - rect.y), (int) Math.max(1, siz * 6 * 0.8), (int) Math.max(1, siz * 6 * 0.8), c[0], c[1], c[2], opacities.get(i));
         }
+
+        g.setComposite(FakeGraphics.DEF, 255, 0);
 
         g.setTransform(at);
         g.delete(at);
@@ -71,7 +73,7 @@ public class StarBackgroundEffect extends BackgroundEffect {
         }
 
         if(capture.size() > 0) {
-            rangeH = sb.h - BGHeight + Data.BG_EFFECT_STAR_Y_RANGE;
+            int rangeH = sb.h - BGHeight + Data.BG_EFFECT_STAR_Y_RANGE;
 
             for(int i = 0; i < capture.size(); i++) {
                 opacities.set(capture.get(i), 0);
@@ -96,16 +98,15 @@ public class StarBackgroundEffect extends BackgroundEffect {
         colors.clear();
         capture.clear();
 
-        rangeH = sb.h - BGHeight + Data.BG_EFFECT_STAR_Y_RANGE;
+        int rangeH = sb.h - BGHeight + Data.BG_EFFECT_STAR_Y_RANGE;
 
         number = sb.st.len / 100;
-        len = sb.st.len;
 
         for(int i = 0; i < number; i++) {
             int time = (int) (sb.r.nextDouble() * Data.BG_EFFECT_STAR_TIME);
 
             opacities.add((int) (255 * Math.sin(Math.PI  * time / 20)));
-            positions.add(P.newP(sb.r.nextDouble() * sb.st.len, sb.r.nextDouble() * rangeH));
+            positions.add(P.newP(sb.r.nextDouble() * (sb.st.len + battleOffset), sb.r.nextDouble() * rangeH));
             colors.add((byte) (sb.r.nextDouble() * (starColors.length - 1)));
             times.add(time);
         }
