858434519bd6db539e0a66c1c911d45c0d4601ba
iREDMe <foodrickme@gmail.com>
2021-10-24 09:29:07 -0700

    custom soul name

--- util/pack/Soul.java
+++ util/pack/Soul.java
@@ -1,6 +1,7 @@
 package common.util.pack;
 
 import common.io.json.JsonClass;
+import common.io.json.JsonDecoder;
 import common.io.json.JsonField;
 import common.pack.Identifier;
 import common.pack.IndexContainer.IndexCont;
@@ -13,7 +14,7 @@ import common.util.stage.Music;
 @JsonClass
 @IndexCont(PackData.class)
 @JsonClass.JCGeneric(Identifier.class)
-public class Soul extends Animable<AnimU<?>, AnimU.UType> implements Comparable<Soul>, Indexable<PackData, Soul> {
+public class Soul extends Animable<AnimU<?>, AnimU.UType> implements Indexable<PackData, Soul> {
 
 	@JsonClass.JCIdentifier
 	@JsonField
@@ -21,6 +22,9 @@ public class Soul extends Animable<AnimU<?>, AnimU.UType> implements Comparable<
 
 	@JsonField
 	public Identifier<Music> audio;
+
+	@JsonField
+	public String name;
 
 	@JsonClass.JCConstructor
 	public Soul() {
@@ -30,6 +34,11 @@ public class Soul extends Animable<AnimU<?>, AnimU.UType> implements Comparable<
 	public Soul(Identifier<Soul> id, AnimU<?> animS) {
 		anim = animS;
 		this.id = id;
+
+		if (id.pack.equals(Identifier.DEF))
+			name = "soul " + id.id;
+		else
+			name = "custom soul " + id.id;
 	}
 
 	@Override
@@ -39,15 +48,7 @@ public class Soul extends Animable<AnimU<?>, AnimU.UType> implements Comparable<
 
 	@Override
 	public String toString() {
-		if (id.pack.equals(Identifier.DEF))
-			return "soul " + id.id;
-		else
-			return "custom soul " + id.id;
-	}
-
-	@Override
-	public int compareTo(Soul o) {
-		return id.compareTo(o.id);
+		return name;
 	}
 
 	@Override
