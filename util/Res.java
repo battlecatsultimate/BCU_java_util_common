df0201c1b1e784dfb1875b171af4bb5cd1947416
iREDMe <foodrickme@gmail.com>
2022-12-25 20:44:10 -0800

    update xp imgcut

--- util/Res.java
+++ util/Res.java
@@ -122,7 +122,7 @@ public class Res extends ImgCore {
 			aux.gatyaitem.put(i, new VImg("./org/page/catfruit/gatyaitemD_" + i + "_f.png"));
 
 		ImgCut xp = new ImgCut();
-		xp.cuts = new int[][] { { 28, 44, 72, 40 } }; // TODO may need to un-hardcode this
+		xp.cuts = new int[][] { { 27, 43, 74, 42 } }; // TODO may need to un-hardcode this
 		aux.XP = new VImg("./org/page/catfruit/xp.png");
 		aux.XP.setCut(xp);
 
