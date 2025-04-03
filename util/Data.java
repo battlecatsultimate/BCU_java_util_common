065fc45e3477f04f9a4f16e145a1039d3e5ad8b0
Gootzi <124401935+ramen-cat-coder@users.noreply.github.com>
2025-04-03 02:37:30 -0700

    Adrenaline talent

--- util/Data.java
+++ util/Data.java
@@ -1378,6 +1378,7 @@ public class Data {
 			{ 0, P_BSTHUNT, 2, -1 }, //64 : beheoth hunter
 			{ 0, P_MINIVOLC, 4, -1 }, //65 : Mini surge
 			{ 1, AB_SKILL, 0, -1 }, //66 : super sage hunter
+			{ 0, P_SPEEDUP, 2, -1 }, //67 : ADRENALINE
 	};
 
 	// foot icon index used in battle
